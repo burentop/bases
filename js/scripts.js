3ced96dc74995949fa0f20c64584e6c2496d809c
Brent Perry <burentoperii@gmail.com>
2016-12-19 16:36:23 -0600

    return input number unconverted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-
+var convertNumber = function(number) {
+  return number;
+}
 
 $(document).ready(function() {
   $("form#base").submit(function(event) {
     event.preventDefault();
     var number = $("#number").val();
     var result = convertNumber(number);
+    $("#output-original").text(number);
+    $("#output-decimal").text(result);
     $("#result").show();
   });
 });
