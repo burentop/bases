90adcf42581e2cf8f7ac1d81a7313fc7c7ac8f70
Brent Perry <burentoperii@gmail.com>
2016-12-19 16:47:48 -0600

    return converted binary number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-var convertNumber = function(number) {
-  return number;
+var convertNumber = function(number, base) {
+  var output = -1;
+  for (var index = 0; index < number.length; index += 1) {
+    var digit = parseInt(number[index]);
+    for (var counter = 0; counter <= digit || counter <= base; counter += 1) {
+      output += 1;
+    }
+  }
+  return output;
 }
 
 $(document).ready(function() {
   $("form#base").submit(function(event) {
     event.preventDefault();
     var number = $("#number").val();
-    var result = convertNumber(number);
+    var base = parseInt($("#base").val());
+    var result = convertNumber(number, base);
     $("#output-original").text(number);
     $("#output-decimal").text(result);
     $("#result").show();
